1cc54dc6d0808da63573e68576e77a1440e8794b
Jason Worden <wordenjasonp@gmail.com>
2018-03-28 01:16:06 -0700

    easing up and down

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const objectPath = require('object-path');
 const CsvReader = require('promised-csv');
 const reader = new CsvReader();
 
+const DEFAULT_START_VOLUME = 25;
+
 // returns promise:
 const getTrackList = () => reader.read('songs.csv', (data) => {
   const min = _.toInteger(data[1]);
@@ -22,26 +24,70 @@ const getRandomTrackFromList = async () => {
   return _.sample(trackList);
 };
 
-const run = async () => {
-  console.log('Standing up!');
-  await spotify.ascertainIsOpened();
-  await spotify.ascertainShuffleState(true);
-
-  const { uri, startingPoint } = await getRandomTrackFromList();
-  await spotify.playTrack(uri);
-
-  await spotify.pause();
-  // give time for new song to register so we can jump to desired spot successfully:
-  await Promise.delay(1000);
-  await spotify.jumpTo(startingPoint);
-  await spotify.play();
-
+const showCurrentTrackNotification = async () => {
   const track = await spotify.getTrack();
   notifier.notify({
     title: 'Stand up!',
     message: `${track.name} by ${track.artist}`,
     icon: 'https://secure.static.tumblr.com/325cf030a255dd186faa6aec64f555d2/k3htaqs/Dxynz1uj7/tumblr_static_spotify-logo_128.png',
   });
+};
+
+const increaseVolumeWithEasing = async ({
+  duration = 8 * 1000,
+  intervalsCount = 10,
+  startVolume = DEFAULT_START_VOLUME,
+  endVolume = 100,
+} = {}) => {
+  await spotify.setVolume(startVolume);
+  const intervalDuration = duration / intervalsCount;
+  for (let i = 0; i <= intervalsCount; i++) {
+    const volume = startVolume + (i / intervalsCount) * (endVolume - startVolume);
+    await spotify.setVolume(volume);
+    await Promise.delay(intervalDuration);
+  }
+};
+
+const decreaseVolumeFromMaxWithEasing = async ({
+  duration = 10 * 1000,
+  intervalsCount = 10,
+  endVolume = 0,
+} = {}) => {
+  const startVolume = 100;
+  const intervalDuration = duration / intervalsCount;
+  for (let i = 0; i <= intervalsCount; i++) {
+  // for (let i = intervalsCount; i >= 0; i--) {
+    const volume = startVolume - (i / intervalsCount) * (startVolume - endVolume);
+    await spotify.setVolume(volume);
+    await Promise.delay(intervalDuration);
+  }
+};
+
+
+const run = async () => {
+  await spotify.pause();
+
+  console.log('Standing up!');
+  await spotify.ascertainIsOpened();
+
+  const { uri, startingPoint } = await getRandomTrackFromList();
+  await spotify.playTrack(uri);
+
+  await spotify.pause();
+  // await spotify.muteVolume();
+  // give time for new song to register so we can jump to desired spot successfully:
+  await Promise.delay(1 * 1000);
+  await spotify.jumpTo(startingPoint);
+  // await spotify.unmuteVolume();
+  await spotify.setVolume(DEFAULT_START_VOLUME);
+  await spotify.play();
+
+  await Promise.all([
+    showCurrentTrackNotification(),
+    increaseVolumeWithEasing(),
+  ]);
+  await Promise.delay(22 * 1000);
+  await decreaseVolumeFromMaxWithEasing();
 };
 
 Promise.resolve()
